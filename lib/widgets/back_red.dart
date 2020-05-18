592249eed8185fe759bb159f420e51b49f177bd2
NeshGogo <rafaelaguero10@hotmail.com>
2020-05-17 21:44:22 -0400

    creating the personalized header of application

--- lib/widgets/back_red.dart
+++ lib/widgets/back_red.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class BackRed extends StatelessWidget {
+  final double height;
+  final String title;
+
+  BackRed({
+    Key key,
+    @required this.title,
+    this.height,
+  });
+
   @override
   Widget build(BuildContext context) {
+    final widthScreen =MediaQuery.of(context).size.width;
     // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      height: height,
+      width: widthScreen,
+      padding: EdgeInsets.only(top:30, left:20, right: 20),
+      color: Colors.red,
+      child: Text(
+        title,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 30
+        ),
+      ),
+    );
   }
   
 }
